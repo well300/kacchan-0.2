d7dc2381808f3e23f0ab49b6bad894d70f67aead
00300 <90952986+well300@users.noreply.github.com>
2021-11-30 22:41:55 +0530

    〽💣

--- src/commands/Media/Take.ts
+++ src/commands/Media/Take.ts
@@ -30,8 +30,8 @@ export default class Command extends BaseCommand {
         if (!buffer) return void M.reply(`You didn't provide any sticker to convert`)
         const filename = `${tmpdir()}/${Math.random().toString(36)}`
         const sticker:any = await  new Sticker(buffer, {
-            pack: pack[1] || '☯️ For You ', 
-            author: pack[2] || 'By JJ-OREKI ☯️', 
+            pack: pack[1] || '〽 ꜰᴏʀ-ʏᴏᴜ', 
+            author: pack[2] || '🐻', 
             quality: 50 
         }).build();
           fs.writeFileSync(`${filename}.webp`,sticker);
