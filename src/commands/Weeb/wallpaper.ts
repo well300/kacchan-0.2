4a213b9081b83dacf5d4b04520f135207a4da4d6
00300 <90952986+well300@users.noreply.github.com>
2021-11-30 21:59:38 +0530

    💣〽

--- src/commands/Weeb/wallpaper.ts
+++ src/commands/Weeb/wallpaper.ts
@@ -40,7 +40,7 @@ export default class Command extends BaseCommand {
 				`Couldn't find any matching term of wallpaper.`
 			));
 		for (let i = 0; i < amount; i++) {
-			const res = `*🤍 Here you go.*`;
+			const res = `*〽 Here you go.*`;
 			this.client.sendMessage(
 				M.from,
 				{ url: wallpaper[i].image },
